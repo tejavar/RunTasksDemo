31486dc3e2e1d05b94754ed704f8e6e2bd8407b7
tejavar <54866295+tejavar@users.noreply.github.com>
2022-04-27 17:00:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,15 +17,22 @@ provider "google" {
 resource "google_compute_firewall" "default" {
   name    = "test-firewall"
   network = google_compute_network.default.name
-
-  allow {
-    protocol = "icmp"
+  
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000"]
-  }
+ ## allow {
+ ##   protocol = "icmp"
+ ## }
+
+ ## allow {
+ ##   protocol = "tcp"
+ ##   ports    = ["80", "8080", "1000-2000"]
+ ## }
 
   source_tags = ["web"]
 }
