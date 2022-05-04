5a3c77a5c64829b16068976339aceaf07ea6d469
tejavar <tejavar@gmail.com>
2022-05-04 17:54:11 -0400

    new beginning

--- main.tf
+++ main.tf
@@ -1,28 +1,27 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.19.0"
     }
   }
 }
 
-terraform{
+terraform {
 
 
-        cloud{
-        organization="terraform-tryout"
+  cloud {
+    organization = "terraform-tryout"
 
-        workspaces{
-                name="runtaskdemo"
-        }
-}
+    workspaces {
+      name = "runtaskdemo"
+    }
+  }
 }
 
 
 
 provider "google" {
-  credentials = file("./tmp/credentials2.json")
 
   project = "hc-327f90383fff489b9447e89f257"
   region  = "us-central1"
@@ -39,8 +38,8 @@ resource "google_compute_firewall" "default" {
     ports    = ["22"]
   }
 
- source_ranges = ["192.168.1.0/24"]
-//  source_ranges = ["0.0.0.0/0"]
+  source_ranges = ["192.168.1.0/24"]
+  //  source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_network" "default" {
