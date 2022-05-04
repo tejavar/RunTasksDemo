5584b542ea0d6ce4c18d8ce1ce8d621bcc2ad39d
tejavar <54866295+tejavar@users.noreply.github.com>
2022-05-04 10:03:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,8 +30,40 @@ resource "google_compute_network" "default" {
   name = "test-network"
 }
 
+resource "google_storage_bucket" "static-site" {
+  name          = "image-store.com"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
 
 
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
 
 
 
