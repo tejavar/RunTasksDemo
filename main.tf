fc0d4446b9ac901b87ae5d841ed1e3ceb8703db7
tejavar <54866295+tejavar@users.noreply.github.com>
2022-05-04 17:14:16 -0400

    Update main.tf
    Opening it up for everyone

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ resource "google_compute_firewall" "default" {
     ports    = ["22"]
   }
 
- source_ranges = ["192.168.1.0/24"]
-//  source_ranges = ["0.0.0.0/0"]
+ //source_ranges = ["192.168.1.0/24"]
+ source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_network" "default" {
