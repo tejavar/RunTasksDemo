776bb130ec09c7b84c4961809243364e29bc36c2
tejavar <tejavar@gmail.com>
2022-05-05 10:18:41 -0400

    new changes to secuirty

--- main.tf
+++ main.tf
@@ -35,11 +35,12 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+      ports    = ["22"]
+ //     ports = ["80", "8080", "1000-2000"]
   }
 
-// source_ranges = ["192.168.1.0/24"]
-   source_ranges = ["0.0.0.0/0"]
+ //  source_ranges = ["192.158.28.0/22"]
+     source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_network" "default" {
