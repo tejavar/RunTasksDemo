018ad1d90b97f1f291c84b451fdbb3a626fdd196
tejavar <tejavar@gmail.com>
2022-05-05 09:15:35 -0400

    changes to storage bucket

--- main.tf
+++ main.tf
@@ -39,47 +39,37 @@ resource "google_compute_firewall" "default" {
   }
 
 // source_ranges = ["192.168.1.0/24"]
-  source_ranges = ["0.0.0.0/0"]
+   source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_network" "default" {
   name = "test-network"
 }
 
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "EU"
+resource "google_storage_bucket" "my-bucket" {
+  name          = "my-bucket-999"
+  location      = "europe-west2"
   force_destroy = true
 
   uniform_bucket_level_access = true
 
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
 }
 
 
-resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
-  location      = "US"
-  force_destroy = true
+//resource "google_storage_bucket" "auto-expire" {
+//  name          = "auto-expiring-bucket"
+//  location      = "US"
+//  force_destroy = true
 
-  lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
-    }
-  }
-}
+// lifecycle_rule {
+//   condition {
+//      age = 3
+//    }
+//    action {
+//      type = "Delete"
+//    }
+//  }
+//}
 
 
 
